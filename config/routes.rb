a6595da639de42c7d20584b3a7f3656174b9c2ee
Tony Huynh <t.hnh@tuta.io>
2019-06-16 21:37:39 +1000

    configured root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  root 'demo#index'
+
   get 'demo/index'
+
+  # default route
+  # may go away in future versions
+  # get ':controller(/:action(/:id))'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
