fe52e96a25d2c1df1dcce7a34db3aa7b172421a4
Tony Huynh <t.hnh@tuta.io>
2019-06-21 18:56:58 +1000

    added instance variable

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,10 +4,15 @@ class DemoController < ApplicationController
   end
 
   def hello
+    @array = [1,2,3,4,5]
     render(:template => 'demo/hello')
   end
 
   def other_hello
     redirect_to(:action => 'index')
+  end
+
+  def lynda
+    redirect_to('https://www.lynda.com/')
   end
 end
