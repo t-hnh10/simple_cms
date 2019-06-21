2e8008a022e0cef813c12d7f4f2ccc96b50bf0f5
Tony Huynh <t.hnh@tuta.io>
2019-06-21 17:39:32 +1000

    rendered template & added redirect

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,13 @@
 class DemoController < ApplicationController
   def index
+    render(:template => 'demo/index')
+  end
+
+  def hello
+    render(:template => 'demo/hello')
+  end
+
+  def other_hello
+    redirect_to(:action => 'index')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'demo#index'
 
   get 'demo/index'
-
+  get 'demo/hello'
+  get 'demo/other_hello'
+  
   # default route
   # may go away in future versions
   # get ':controller(/:action(/:id))'
