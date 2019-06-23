e5e28a7f55a61185da2eb3c6c6a95f0fbaa41b5f
Tony Huynh <t.hnh@tuta.io>
2019-06-24 00:13:21 +1000

    added URL parameters

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -5,6 +5,8 @@ class DemoController < ApplicationController
 
   def hello
     @array = [1,2,3,4,5]
+    @id = params['id']
+    @page = params[:page]
     render(:template => 'demo/hello')
   end
 
