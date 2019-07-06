714c61ae1949074d85b76c99a2ad74375d3f2392
Tony Huynh <t.hnh@tuta.io>
2019-07-07 00:27:05 +1000

    generated migration for new table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_233700) do
+ActiveRecord::Schema.define(version: 2019_07_06_135353) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name", limit: 25
     t.string "last_name", limit: 50
-    t.string "email", default: "", null: false
-    t.string "password", limit: 40
+    t.string "email", limit: 100, default: "", null: false
+    t.string "username", limit: 25
+    t.string "hashed_password", limit: 40
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_admin_users_on_username"
   end
 
 end
